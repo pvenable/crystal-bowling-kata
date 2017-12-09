6ec06f98b116caba49d251a26765547c9cd1081f
Paul Venable <pvenable@gmail.com>
2017-12-09 09:07:27 -0800

    Refactor bonus code

--- src/game.cr
+++ src/game.cr
@@ -13,10 +13,10 @@ class Game
 
     10.times do |frame_index|
       if strike?(roll_index)
-        total += 10 + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+        total += strike_bonus(roll_index)
         roll_index += 1
       elsif spare?(roll_index)
-        total += 10 + @rolls[roll_index + 2]
+        total += 10 + spare_bonus(roll_index)
         roll_index += 2
       else
         total += @rolls[roll_index] + @rolls[roll_index + 1]
@@ -31,7 +31,15 @@ class Game
     @rolls[roll_index] + @rolls[roll_index + 1] == 10
   end
 
+  private def spare_bonus(roll_index)
+    @rolls[roll_index + 2]
+  end
+
   private def strike?(roll_index)
     @rolls[roll_index] == 10
+  end
+
+  private def strike_bonus(roll_index)
+    10 + @rolls[roll_index + 1] + @rolls[roll_index + 2]
   end
 end
